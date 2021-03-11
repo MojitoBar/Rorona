9b54b9e4b28e09fb0118078fdb18cae5ef429baf
judongseok <wnehdtjr5@naver.com>
2021-03-12 00:50:07 +0900

    Initial Commit

---
+++ Shared/roronaApp.swift
@@ -0,0 +1,17 @@
+//
+//  roronaApp.swift
+//  Shared
+//
+//  Created by judongseok on 2021/03/12.
+//
+
+import SwiftUI
+
+@main
+struct roronaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

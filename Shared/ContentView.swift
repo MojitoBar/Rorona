37c964f9251c380a028b72a78d5e90b9cce6c2b3
judongseok <wnehdtjr5@naver.com>
2021-03-14 00:08:20 +0900

    add design

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,52 +8,103 @@
 import SwiftUI
 
 struct CardView: View {
+    var colors: [Color]
+    var image: String
+    let nanum = UIFont(name: "NanumSquareOTF_acB", size: 10)
     var roronainfo: RoronaInfo
     var body: some View {
         ZStack{
-            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+            RoundedRectangle(cornerRadius: 15.0)
                 .fill(
-                    LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.04076000323, green: 0.2159992264, blue: 0.372004512, alpha: 1)), Color(#colorLiteral(red: 0.06490538184, green: 0.343952678, blue: 0.5923722519, alpha: 1))]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                    LinearGradient(gradient: Gradient(colors: [colors[0], colors[1]]), startPoint: .topLeading, endPoint: .bottomTrailing)
                 )
                 .frame(width: 300, height: 200)
+                .shadow(color: colors[0], radius: 5, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/)
                 .padding()
-                .shadow(color: Color(#colorLiteral(red: 0.101288268, green: 0.1096670212, blue: 0.1218392683, alpha: 1)), radius: 5, x: 0, y: 0)
+            ZStack{
+                Circle()
+                    .opacity(0.25)
+                    .blur(radius: 4)
+                    .foregroundColor(Color(#colorLiteral(red: 0.9796831018, green: 0.9796831018, blue: 0.9796831018, alpha: 1)))
+                Image(image)
+                    .resizable()
+                    .frame(width: 65, height: 65)
+                    .padding(.init(top: -10, leading: 20, bottom: 0, trailing: 0))
+            }
+            .frame(width: 110, height: 110)
+            .padding(.init(top: -140, leading: -165, bottom: 0, trailing: 0))
+            
             VStack(alignment: .leading){
-                Text(roronainfo.name)
-                    .font(.system(size: 35))
-                    .frame(width: 250, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: .leading)
-                    .foregroundColor(.white)
-                Text(roronainfo.rule)
-                    .foregroundColor(.white)
-                Text(roronainfo.date)
-                    .foregroundColor(.white)
-                Spacer()
+                VStack(alignment: .leading){
+                    Text(roronainfo.name)
+                        .font(.custom("NanumSquareOTF_acEB", size: 35))
+                        .frame(width: 220, height: 100, alignment: .leading)
+                        .foregroundColor(.white)
+                }
+                .padding(.leading, 140)
+                VStack(alignment: .leading){
+                    Text(roronainfo.rule)
+                        .font(.custom("NanumSquareOTF_acB", size: 25))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding(.init(top: 5, leading: 0, bottom: 10, trailing: 0))
+                    Text(roronainfo.date)
+                        .font(.custom("NanumSquareOTF_acB", size: 20))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                    Spacer()
+                }
+                .padding(.leading, 50)
             }
         }
+        .padding(.top, 25)
     }
 }
 
 struct ContentView: View {
+    
+    // db에 추가하기
+    @State var Colors = [
+        [Color(#colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)), Color(#colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1))],
+        [Color(#colorLiteral(red: 0.2386925334, green: 0.4814667995, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.5587813862, green: 0.6104271209, blue: 0.9764705896, alpha: 1))],
+        [Color(#colorLiteral(red: 0.9212490338, green: 0.273721212, blue: 0.5218184406, alpha: 1)), Color(#colorLiteral(red: 1, green: 0.5301271778, blue: 0.7130295974, alpha: 1))],
+        [Color(#colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)), Color(#colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1))],
+        [Color(#colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)), Color(#colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1))],
+        [Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1))],
+        [Color(#colorLiteral(red: 0.9796831018, green: 0.9796831018, blue: 0.9796831018, alpha: 1)), Color(#colorLiteral(red: 0.9796831018, green: 0.9796831018, blue: 0.9796831018, alpha: 1))]
+    ]
+    @State var Image = [
+        "party",
+        "mic",
+        "computer",
+        "party",
+        "party",
+        "party"
+    ]
+    
     @ObservedObject var roronainfo = FetchUser()
     
-    init() {
-        UITableView.appearance().separatorColor = .clear
-    }
-    
     var body: some View {
-        VStack{
-            HStack{
-                Text("RORONA!")
-                    .padding(.leading, 30)
-                    .font(.system(size: 30))
-                Spacer()
-                Circle()
-                    .frame(width: 40, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                    .padding(.trailing, 30)
-            }
-            NoSepratorList{
-                ForEach(roronainfo.roronainfos){roronainfo in
-                    CardView(roronainfo: roronainfo)
+        ZStack{
+            Color(#colorLiteral(red: 0.9796831018, green: 0.9796831018, blue: 0.9796831018, alpha: 1))
+                .ignoresSafeArea()
+            VStack{
+                HStack{
+                    Text("RORONA!")
+                        .fontWeight(.bold)
+                        .padding(.leading, 30)
+                        .font(.system(size: 30))
+                        .foregroundColor(Color(#colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1)))
+                    Spacer()
+                    Circle()
+                        .frame(width: 40, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .padding(.trailing, 30)
+                        .foregroundColor(Color(#colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1)))
+                }
+                NoSepratorList{
+                    ForEach(roronainfo.roronainfos){roronainfo in
+                        CardView(colors: Colors[roronainfo.id - 1], image: Image[roronainfo.id - 1], roronainfo: roronainfo)
+                    }
                 }
             }
         }
